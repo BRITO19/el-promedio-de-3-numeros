f79094b5c5c7c6f12c73c000ca4abf1745b6c640
Gustavo Brito Reynoso <74156487+BRITO19@users.noreply.github.com>
2024-09-03 20:22:40 -0400

    v1

---
+++ main.js
@@ -0,0 +1,12 @@
+
+alert("Bienvenido a Teorema")
+const num1=prompt("Ingrese el primer numero")
+const num1Int=Number(num1)
+const num2=prompt("Ingrese el segundo numero")
+const num2Int=Number(num2)
+const num3=prompt("Ingrese el tercer numero")
+const num3Int=Number(num3)
+
+const resultado=(num1Int+num2Int+num3Int)/3
+
+alert("el promedio de los tres numero es: " + resultado )
